a88efe5121217b0cd35574ccaa42e053d7bb68f6
Luis F Pacheco <pluisfer@gmail.com>
2021-04-16 15:53:18 -0500

    update ciclos.cpp
    upgrade

--- ciclos.cpp
+++ ciclos.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int32_t main()
+/* int32_t main()
 {
     int32_t numRepeticiones = 3;
     while (numRepeticiones > 0)
@@ -9,5 +9,19 @@ int32_t main()
 
         numRepeticiones = numRepeticiones - 1;
     }
+    return 0;
+} */
+
+int32_t main()
+{
+    int32_t numRepeticiones = 0;
+
+    do
+    {
+        std::cout << "Aprendiendo a usar bucles while." << std::endl;
+
+        numRepeticiones = numRepeticiones - 1;
+    } while (numRepeticiones > 0);
+
     return 0;
 }
