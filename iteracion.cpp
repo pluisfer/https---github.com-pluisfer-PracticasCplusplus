c4f509b8a92625822a40bed9eb267c4039fe59a2
Luis F Pacheco <pluisfer@gmail.com>
2021-04-16 16:15:47 -0500

    creacion archivo iteracion
    se crea archivo iteracion.cpp en donde estan ejemplo de ciclos for sencillo y en vectores

--- iteracion.cpp
+++ iteracion.cpp
@@ -4,10 +4,22 @@
 int32_t main()
 {
 
-    for (int contador = 1; contador <= 3; contador++)
+    for (int32_t contador = 1; contador <= 3; contador++)
     {
 
         std::cout << "Aprendiendo a utlizar ciclos for." << std::endl;
+    }
+
+    std::vector<int32_t> valores(5);
+    valores[0] = 1;
+    valores[1] = 2;
+    valores[2] = 3;
+    valores[3] = 4;
+    valores[4] = 5;
+
+    for (int32_t i = 0; i < 5; i++)
+    {
+        std::cout << "El valor contenido en la posicion" << i << "el vector es" << valores.at(i) << std::endl;
     }
 
     return 0;
