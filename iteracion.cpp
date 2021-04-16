b1e16f11179af3660a42f961bd4183f1d2ae5511
Luis F Pacheco <pluisfer@gmail.com>
2021-04-16 16:03:12 -0500

    upgrade ciclos
    upgrade

---
+++ iteracion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int32_t main()
+{
+
+    for (int contador = 1; contador <= 3; contador++)
+    {
+
+        std::cout << "Aprendiendo a utlizar ciclos for." << std::endl;
+    }
+
+    return 0;
+}
